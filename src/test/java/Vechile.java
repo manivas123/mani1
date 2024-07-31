4a31b90c9475a7ee96fba1f8cc4de554be2301ab
manivas123 <manivasagam9521@gmail.com>
2024-07-31 18:57:24 +0530

    taskfile

---
+++ src/test/java/Vechile.java
@@ -0,0 +1,22 @@
+
+public class Vechile {
+
+	
+	private void Honda() {
+
+		System.out.println("1");
+		
+		
+	}
+	private void Fz() {
+		System.out.println("2");
+
+	}
+	private void susuki() {
+        System.out.println("3");
+		
+	}
+	
+	
+	
+}
